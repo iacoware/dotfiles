1db3efd891f774fc031652bebaa1b27c918ed5a0
Massimo Iacolare <info@massimoiacolare.it>
2015-12-21 08:47:43 +0100

    Initial commit

---
+++ brew.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+####################################################
+echo "installing brew formulaes"
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+brew install tree
+brew install z
+
+####################################################
+echo "installing cask apps"
+
+# Install Cask
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# Core casks
+#brew cask install --appdir="/Applications" alfred
+#brew cask install --appdir="~/Applications" iterm2
+#brew cask install --appdir="~/Applications" java
+#brew cask install --appdir="~/Applications" xquartz
+
+# Development tool casks
+brew cask install --appdir="/Applications" sublime-text3
+#brew cask install --appdir="/Applications" atom
+#brew cask install --appdir="/Applications" virtualbox
+#brew cask install --appdir="/Applications" vagrant
+#brew cask install --appdir="/Applications" heroku-toolbelt
+#brew cask install --appdir="/Applications" macdown
+
+# Misc casks
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" lastpass
+#brew cask install --appdir="/Applications" skype
+#brew cask install --appdir="/Applications" slack
+#brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" vlc
+#brew cask install --appdir="/Applications" gimp
+#brew cask install --appdir="/Applications" inkscape
+
+# Link cask apps to Alfred
+#brew cask alfred link
+
+# Install Docker, which requires virtualbox
+#brew install docker
+#brew install boot2docker
+
+# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+
+######################################################
+echo "Install cask fonts"
+brew tap caskroom/fonts
+brew cask install font-bebas-neue
+
+brew cleanup
+brew cask cleanup --outdated
