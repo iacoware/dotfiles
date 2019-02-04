f7677190495b8109ec4d52c5f8c673a9e6588cc0
Massimo Iacolare <info@massimoiacolare.it>
2019-02-04 18:38:52 +0100

    Add k abbr to cd into katas directory

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,4 @@ set -U fish_user_paths $fish_user_paths /usr/local/opt/coreutils/libexec/gnubin
 set -U fish_user_paths $fish_user_paths ./node_modules/.bin
 
 abbr --add p cd ~/projects
+abbr --add k cd ~/projects/katas
