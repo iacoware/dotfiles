4daef0e53a2485abb018095f6d419ebb1974b96c
Massimo Iacolare <info@massimoiacolare.it>
2019-02-04 18:56:23 +0100

    Fish: select which nodejs version to start

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 set pure_symbol_prompt "~>"
 
+#Add more directory to PATH
 set -U fish_user_paths $fish_user_paths /usr/local/opt/coreutils/libexec/gnubin
 set -U fish_user_paths $fish_user_paths ./node_modules/.bin
 
 abbr --add p cd ~/projects
 abbr --add k cd ~/projects/katas
+
+nvm use default
