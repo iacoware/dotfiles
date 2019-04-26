d44320433234f44c4e1454f90452062b3b2562d9
Massimo Iacolare <info@massimoiacolare.it>
2019-04-26 19:33:24 +0200

    Hammerspoon: added setup file. Symlink init.lua and Spoons directory

---
+++ hammerspoon/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+files=(
+	init.lua
+)
+
+for file in ${files[@]}; do
+	[ -r "$file" ] && [ -f "$file" ] && ln -sfn ${BASEDIR}/"$file" ~/.hammerspoon/"$file";
+done;
+
+ln -sfn ${BASEDIR}/Spoons ~/.hammerspoon/
