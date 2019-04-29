d1e843ceb6d78a75b28092f8d0fc6de7e3e7f399
Massimo Iacolare <info@massimoiacolare.it>
2019-04-29 10:06:25 +0200

    Hammerspoon win-mgr: added curried version of move, resize, snap function for better commands readability

--- hammerspoon/win-mgr.lua
+++ hammerspoon/win-mgr.lua
@@ -5,6 +5,36 @@ local moveIncrement = 30
 local sizeIncrement = 30
 local prevFrame = {}
 
+function moveCurried(xInc, yInc)
+    return function() moveWindow(xInc, yInc) end
+end
+
+function resizeCurried(wInc, hInc)
+    return function() resizeWindow(wInc, hInc) end
+end
+
+function snapCurried(xf, yf, wf, hf)
+    return function() snapWindow(xf, yf, wf, hf) end
+end
+
+-- Move windows ← ↑ ↓ →
+hs.hotkey.bind(hyperMove, "Up",     moveCurried(0, -moveIncrement))
+hs.hotkey.bind(hyperMove, "Down",   moveCurried(0, moveIncrement))
+hs.hotkey.bind(hyperMove, "Left",   moveCurried(-moveIncrement, 0))
+hs.hotkey.bind(hyperMove, "Right",  moveCurried(moveIncrement, 0))
+
+-- Resize windows ← ↑ ↓ →
+hs.hotkey.bind(hyperResize, "Right",resizeCurried(sizeIncrement, 0))
+hs.hotkey.bind(hyperResize, "Left", resizeCurried(-sizeIncrement, 0))
+hs.hotkey.bind(hyperResize, "Down", resizeCurried(0, sizeIncrement))
+hs.hotkey.bind(hyperResize, "Up",   resizeCurried(0, -sizeIncrement))
+
+--Snap windows left & right, maximize, center
+hs.hotkey.bind(hyperSnap, "Left",   snapCurried(0, 0, 0.5, 1))
+hs.hotkey.bind(hyperSnap, "Right",  snapCurried(0.5, 0, 0.5, 1))
+hs.hotkey.bind(hyperSnap, "C",      snapCurried(0.25/2, 0.25/2, 0.75, 0.75))
+hs.hotkey.bind(hyperSnap, "F",      snapCurried(0, 0, 1, 1))
+
 -- Move to next monitor
 hs.hotkey.bind(hyperMove, "M", function()
     local win = hs.window.focusedWindow()
@@ -12,58 +42,6 @@ hs.hotkey.bind(hyperMove, "M", function()
     local nextScreen = screen:next()
     win:moveToScreen(nextScreen)
     hs.alert.show("Move to next screen")
-end)
-
--- Move windows ← ↑ ↓ →
-hs.hotkey.bind(hyperMove, "Up", function()
-    moveWindow(0, -moveIncrement)
-end)
-
-
-hs.hotkey.bind(hyperMove, "Down", function()
-    moveWindow(0, moveIncrement)
-end)
-
-hs.hotkey.bind(hyperMove, "Left", function()
-    moveWindow(-moveIncrement, 0)
-end)
-
-hs.hotkey.bind(hyperMove, "Right", function()
-    moveWindow(moveIncrement, 0)
-end)
-
--- Resize windows ← ↑ ↓ →
-hs.hotkey.bind(hyperResize, "Right", function()
-    resizeWindow(sizeIncrement, 0)
-end)
-
-hs.hotkey.bind(hyperResize, "Left", function()
-    resizeWindow(-sizeIncrement, 0)
-end)
-
-hs.hotkey.bind(hyperResize, "Down", function()
-    resizeWindow(0, sizeIncrement)
-end)
-
-hs.hotkey.bind(hyperResize, "Up", function()
-    resizeWindow(0, -sizeIncrement)
-end)
-
---Snap windows left & right, maximize, center
-hs.hotkey.bind(hyperSnap, "Left", function()
-    snapWindow(0, 0, 0.5, 1)
-end)
-
-hs.hotkey.bind(hyperSnap, "Right", function()
-    snapWindow(0.5, 0, 0.5, 1)
-end)
-
-hs.hotkey.bind(hyperSnap, "C", function()
-    snapWindow(0.25/2, 0.25/2, 0.75, 0.75)
-end)
-
-hs.hotkey.bind(hyperSnap, "F", function()
-    snapWindow(0, 0, 1, 1)
 end)
 
 -- Restore prev size and position
